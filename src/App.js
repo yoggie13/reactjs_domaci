102953d848e5b0c6fb590868dd4f903f996a80b7
yoggie13 <57997963+yoggie13@users.noreply.github.com>
2021-02-06 16:04:30 +0100

    First component works

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
 import Select from 'react-select'
+import React,{Component} from 'react'
+import { render } from '@testing-library/react';
 
-const options = [
-  {key: 1, label: "Rock"}, 
-  {key: 2, label: "Hip-hop"},
-  {key: 3, label: "Punk"},
-  {key: 4, label: "Techno"},
-  {key: 5, label: "Pop"}];
+const  genres = [
+  {value: 1, label: "Rock"}, 
+  {value: 2, label: "Hip-hop"},
+  {value: 3, label: "Punk"},
+  {value: 4, label: "Techno"},
+  {value: 5, label: "Pop"}
+];
 
-function App() {
-  return (
-    <div>
-        <h1>Music Finder</h1>
-      <form>
-        <h2>Izaberite žanr:</h2>
-        <Select options = {options}></Select>
-        <input type = "submit" value = "Generiši plejlistu"></input>
-      </form>
-    </div>
-  );
+class Welcome extends Component{
+  constructor(props){
+    super(props);
+    this.onSubmitHandler = this.onSubmitHandler.bind(this);
+    this.state = {
+      selectedGenre : genres[0]
+    }; 
+     
+  this.onSubmitHandler = this.onSubmitHandler.bind(this);
+  this.handleChange = this.handleChange.bind(this);  
+  }
+  onSubmitHandler = () => {
+    alert("Izabrali ste žanr: " + this.state.selectedGenre.label);
+  }
+  handleChange = (e) =>{
+    this.setState({selectedGenre : e});
+  }
+  render(){
+    return (
+      <div>
+          <h1>Music Finder</h1>
+        <form onSubmit = {this.onSubmitHandler}>
+          <h2>Izaberite žanr:</h2>
+          <Select defaultValue = {genres[0]} options = {genres} onChange={this.handleChange}></Select>
+          <input type = "submit" value = "Generiši plejlistu"></input>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default App;
+
+export default Welcome;
